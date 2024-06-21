b8f2dd827ce113fe8e44a8051315affc65ebb664
AJ <ahunter.dev@gmail.com>
2024-06-21 16:14:35 -0500

    theme initalizing

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php 
+
+add_theme_support('title-tag');
+add_theme_support('menus');
+add_theme_support('custom-logo');
+
+
+add_action('wp_enqueue_scripts', 'ct_scripts');
+function ct_scripts() {
+  wp_enqueue_style('style', get_stylesheet_uri());
+  wp_enqueue_script('ct_scripts', get_theme_file_uri('./dist/index.bundled.js'), array(), '1.0', true);
+  wp_enqueue_style('ct_styles', get_theme_file_uri('./dist/index.css'));
+}
